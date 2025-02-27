17a027e0a30096aa99333f62d122658ec65b7bd1
Luke Mossbarger <mossbargerphone@gmail.com>
2025-02-27 13:55:38 -0600

    fixing maxs comments 2

--- src/pysrc/test/int/test_data_client.py
+++ src/pysrc/test/int/test_data_client.py
@@ -12,17 +12,11 @@ def test_integration() -> None:
         assert trade["amount"]
         assert trade["timestampms"]
         assert trade["price"]
-        if (
-            trade["type"]
-            and trade["amount"]
-            and trade["timestampms"]
-            and trade["price"]
-        ):
-            assert (
-                trade["type"] == "trade"
-                or trade["type"] == "buy"
-                or trade["type"] == "sell"
-            )
-            assert type(float(trade["amount"])) is float
-            assert type(trade["timestampms"]) is int
-            assert type(float(trade["price"])) is float
+        assert (
+            trade["type"] == "trade"
+            or trade["type"] == "buy"
+            or trade["type"] == "sell"
+        )
+        assert type(float(trade["amount"])) is float
+        assert type(trade["timestampms"]) is int
+        assert type(float(trade["price"])) is float
