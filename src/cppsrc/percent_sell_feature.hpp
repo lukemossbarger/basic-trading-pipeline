cd3b8bbe39f3019b60b1c6b43d1dca2039146561
Luke Mossbarger <mossbargerphone@gmail.com>
2025-02-28 00:45:57 -0600

    percent sell feature passes tests

---
+++ src/cppsrc/percent_sell_feature.hpp
@@ -0,0 +1,18 @@
+#include "base_feature.hpp"
+
+using namespace intproj;
+
+class PercentSellFeature : public BaseFeature
+{
+
+  public:// tuple is <price, volume, buy> where buy == false means it is a sell
+    PercentSellFeature() {}
+
+    float compute_feature(std::vector<std::tuple<float, float, bool>> data)
+    {
+        float sells = 0;
+        for (int i = 0; i < data.size(); i++) { sells += !std::get<2>(data[i]); }
+        if (!data.size()) { return 0; }
+        return sells / data.size();
+    }
+};
